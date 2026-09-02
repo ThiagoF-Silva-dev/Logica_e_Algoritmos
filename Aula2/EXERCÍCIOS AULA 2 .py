cc0bf7c2f382e3f58acef189ce919b6af8634d38
Thiago Ferreira <ThiagoF.Silva@outlook.com.br>
2026-09-02 17:19:42 -0300

    Organizando pastas por aula

---
+++ Aula2/EXERCÍCIOS AULA 2 .py
@@ -0,0 +1,36 @@
+def main ():
+"""Exercício 1 — Soma de dois números"""
+
+    #pc = float(input("Digite o peso da caixa: \t "))
+    #po = float(input("Digite o peso do objeto: \t "))
+    #cabe = pc-po
+    #print(f"O objeto cabe na caixa: \t ",pc>=po)
+
+"""Exercício 2 — Limite de peso de uma caixa"""
+
+    #numero1 = float(input("Digite o valor do primeiro numéro: \t"))
+    #numero2 = float(input("Digite o valor do segundo numéro: \t"))
+    #valor = numero1+numero2
+    #print(f"A soma dos dois numéros e maior que 20? \t", valor >=20)
+
+"""Exercício 3 — Estoque da farmácia"""
+
+    #unidades = int(input("Digite o número de unidades vendidas: \t"))
+    #estoque = 100-unidades
+    #print(f"Estoque baixo?: \t",estoque<20)
+
+"""Exercício 4 — Controle de velocidade"""
+
+   #velocidade = int(input("Qual a Velocidade do carro?: \t"))
+   #print(f"O carro foi multado? \t",velocidade>80)
+
+"""Exercício 5 — Aprovação do aluno"""
+
+   # nota = float(input("Digite a nota da prova: \t"))
+   #frequencia = float(input("Qual a frequencia do aluno em %: \t"))
+   #aprovado = nota >= 6 and frequencia >=75
+   #print(f"Aluno Aprovado?: \t",aprovado)
+
+
+if __name__ == "__main__":
+    main()
