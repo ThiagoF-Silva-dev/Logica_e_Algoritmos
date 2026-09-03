1fa93a36a41e32bb595f6767d1df40210512e1d3
Thiago Ferreira <thiagof.silva@outlook.com.br>
2026-09-03 16:48:37 -0300

    Adiciona exercicios da Aula 3

---
+++ Aula3/EXERCÍCIOS AULA 3.py
@@ -0,0 +1,79 @@
+def main():
+
+   """Exercício 1 — Elegibilidade para Votação"""
+
+    #nascimento = int(input("Digite a sua data de nascimento: \t "))
+    #ano = int(input("Digite o ano atual: \t "))
+    #idade = ano-nascimento
+
+    #if idade <16:
+        #print(f"Vôce nao pode votar porque ainda e menor de idade! \t")
+    #else:
+        #print(f"Vôce está apto a votar por que ja tem a idade minima! \t")
+
+   """Exercício 2 — Número Positivo ou Negativo"""
+
+   #numero= int(input("Digite um número: \t"))
+
+   #if numero >=0:
+       #print(f"O numero é Positivo! \t")
+   #else:
+       #print(f"O número é Negativo! \t")
+
+   """Exercício 3 — Carteira de Motorista"""
+
+   # idade = int(input(f"Digite sua idade: \t"))
+   #
+   # if idade>= 18:
+   #    cnh = input(f"Vôce, possui CNH (s/n): \t")
+   #    if cnh == "s":
+   #        print(f"Vôce, pode dirigir! \t")
+   #    else:
+   #        print(f"Vôce, Precisa tirar a carteira! \t")
+   # else:
+   #    print(f"Vôce, não pode dirigir! \t")
+
+   """Exercício 4 — Positivo, Negativo, Zero, Par ou Ímpar"""
+
+   # numeInt = int(input("Digite um número inteiro: \t "))
+   #
+   # resto = numeInt % 2
+   #
+   # if numeInt > 0:
+   #     print(f"É Positivo! \t")
+   #     if (resto == 0):
+   #          print(f"É PAR \t ")
+   #     else:
+   #          print(f"É IMPAR \t ")
+   # else:
+   #     if numeInt < 0:
+   #         print(f"Número Negativo! \t ")
+   #     else:
+   #         print(f"Zero não é positivo nem negativo! ")
+
+   """Exercício 5 — Par/Ímpar e Comparação de Dois Números"""
+   #
+   # numero = int(input("Digite um numero inteiro: \t"))
+   #
+   # resto = numero % 2
+   #
+   # if (resto == 0):
+   #     print(f"É PAR! \t")
+   # else:
+   #     print(f"É IMPAR! \t")
+   #
+   # print ("_"*30)
+   #
+   # n1= int(input("Digite o 1° número: \t"))
+   # n2 = int(input("Digite o 2° número: \t"))
+   #
+   # if n1 > n2:
+   #     print(f"{n1} é maior que {n2} \t ")
+   # else:
+   #     if n2 > n1:
+   #         print(str(n2)+" É maior que"+str(n1))
+   #     else:
+   #         print(str(n1)+" É igual a "+str(n1))
+
+if __name__ == "__main__":
+    main()
